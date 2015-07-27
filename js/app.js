e9eb1d5d06913a5a19ec9a4eda588eea57166c17
sohan1 <shn619@gmail.com>
2015-07-27 18:40:54 +0530

    changes

--- js/app.js
+++ js/app.js
@@ -6,13 +6,13 @@ var firstapp = angular.module('firstapp', [
     'navigationservice'
 ]);
 
-firstapp.config(function($stateProvider, $urlRouterProvider, cfpLoadingBarProvider) {
+firstapp.config(function ($stateProvider, $urlRouterProvider, cfpLoadingBarProvider) {
     //Turn the spinner on or off
     cfpLoadingBarProvider.includeSpinner = false;
 
     $stateProvider
 
-    .state('home', {
+        .state('home', {
         url: "/home",
         templateUrl: "views/template.html",
         controller: 'HomeCtrl'
@@ -23,11 +23,28 @@ firstapp.config(function($stateProvider, $urlRouterProvider, cfpLoadingBarProvid
         templateUrl: "views/template.html",
         controller: 'FeatureCtrl'
     })
-    
+
     .state('infinite', {
         url: "/infinite",
         templateUrl: "views/template.html",
         controller: 'InfiniteCtrl'
+    })
+
+    .state('artist', {
+        url: "/artist",
+        templateUrl: "views/template.html",
+        controller: 'ArtistCtrl'
+    })
+
+    .state('artist-detail', {
+        url: "/artist-detail",
+        templateUrl: "views/template.html",
+        controller: 'ArtistDetailCtrl'
+    }) 
+        .state('artist-detail-image', {
+        url: "/artist-detail-image",
+        templateUrl: "views/template.html",
+        controller: 'ArtistDetailImageCtrl'
     })
 
     $urlRouterProvider.otherwise("/home");
