c1bd529a03444bf065a46b607dadc6a6cc535194
jagruti181 <patiljagruti181@gmail.com>
2015-08-04 13:18:29 +0530

    git

--- js/app.js
+++ js/app.js
@@ -6,13 +6,13 @@ var firstapp = angular.module('firstapp', [
     'navigationservice'
 ]);
 
-firstapp.config(function ($stateProvider, $urlRouterProvider, cfpLoadingBarProvider) {
+firstapp.config(function($stateProvider, $urlRouterProvider, cfpLoadingBarProvider) {
     //Turn the spinner on or off
     cfpLoadingBarProvider.includeSpinner = false;
 
     $stateProvider
 
-        .state('home', {
+    .state('home', {
         url: "/home",
         templateUrl: "views/template.html",
         controller: 'HomeCtrl'
@@ -34,12 +34,12 @@ firstapp.config(function ($stateProvider, $urlRouterProvider, cfpLoadingBarProvi
         url: "/cart",
         templateUrl: "views/template.html",
         controller: 'CartCtrl'
-    })  
-        .state('checkout', {
-        url: "/checkout",
-        templateUrl: "views/template.html",
-        controller: 'CheckoutCtrl'
     })
+        .state('checkout', {
+            url: "/checkout",
+            templateUrl: "views/template.html",
+            controller: 'CheckoutCtrl'
+        })
 
     .state('artist', {
         url: "/artist",
@@ -48,10 +48,10 @@ firstapp.config(function ($stateProvider, $urlRouterProvider, cfpLoadingBarProvi
     })
 
     .state('artistdetail', {
-            url: "/ArtistDetail",
-            templateUrl: "views/template.html",
-            controller: 'ArtistDetailCtrl'
-        })
+        url: "/ArtistDetail",
+        templateUrl: "views/template.html",
+        controller: 'ArtistDetailCtrl'
+    })
         .state('detailImage', {
             url: "/detailImage",
             templateUrl: "views/template.html",
@@ -62,16 +62,46 @@ firstapp.config(function ($stateProvider, $urlRouterProvider, cfpLoadingBarProvi
 
 });
 
-firstapp.directive("scroll", function ($window) {
+firstapp.directive("scroll", function($window) {
     return function(scope, element, attrs) {
         angular.element($window).bind("scroll", function() {
-             if (this.pageYOffset >= 100) {
-                 element.addClass('min');
-                 console.log('Scrolled below header.');
-             } else {
-                 element.removeClass('min');
-                 console.log('Header is in view.');
-             }
+            if (this.pageYOffset >= 100) {
+                element.addClass('min');
+                console.log('Scrolled below header.');
+            } else {
+                element.removeClass('min');
+                console.log('Header is in view.');
+            }
         });
+    };
+});
+
+firstapp.directive('fixit',function($window) {
+	return function(scope, element, attrs) {
+		var myelem = {};
+            var imagedim = {};
+            $element = $(element);
+            myelem.height = $element.height();
+            myelem.width = $element.width();
+            myelem.ratio = myelem.width / myelem.height;
+		
+            	$element.children("img.fix-img").load(function() {
+                imagedim.height = $(this).height();
+                imagedim.width = $(this).width();
+                imagedim.ratio = imagedim.width / imagedim.height;
+			  
+			  
+			  if (myelem.ratio == imagedim.ratio) {
+                    $(this).css("width", "auto");
+                    $(this).css("height", myelem.height);
+                } else if (myelem.ratio > imagedim.ratio) {
+                    $(this).css("width", "auto");
+                    $(this).css("height", myelem.height);
+                } else {
+                    $(this).css("width", "100%");
+                    $(this).css("height", "auto");
+                }
+			  
+            });
     };
 });
